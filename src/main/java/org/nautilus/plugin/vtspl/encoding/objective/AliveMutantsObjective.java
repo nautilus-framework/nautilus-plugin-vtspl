8af1b212e698e658d32ed7ab762fb38d3a4f7e5b
Thiago Nascimento <thiagodnf@gmail.com>
2020-09-30 11:35:19 -0400

    added initial source code

---
+++ src/main/java/org/nautilus/plugin/vtspl/encoding/objective/AliveMutantsObjective.java
@@ -0,0 +1,45 @@
+package org.nautilus.plugin.vtspl.encoding.objective;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.nautilus.core.model.Instance;
+import org.nautilus.core.objective.AbstractObjective;
+import org.nautilus.plugin.vtspl.encoding.instance.AbstractTXTInstanceData;
+import org.uma.jmetal.solution.Solution;
+
+public class AliveMutantsObjective extends AbstractObjective {
+
+	protected Set<Integer> deadMutants;
+	
+	@Override
+	public void beforeProcess(Instance instanceData, Solution<?> sol) {
+		this.deadMutants = new HashSet<>();
+	}
+	
+	@Override
+	public void process(Instance instanceData, Solution<?> sol, int i) {
+		
+		AbstractTXTInstanceData instance = (AbstractTXTInstanceData) instanceData;
+		
+		deadMutants.addAll(instance.getMutants(i));
+	}
+	
+	@Override
+	public double calculate(Instance instanceData, Solution<?> sol) {
+
+		AbstractTXTInstanceData instance = (AbstractTXTInstanceData) instanceData;
+
+		return 1.0 - (double) deadMutants.size() / (double) instance.getNumberOfMutants();
+	}
+	
+	@Override
+	public String getName() {
+		return "Alive Mutants";
+	}
+
+	@Override
+	public String getGroupName() {
+		return "General";
+	}
+}
---
+++ src/main/java/org/nautilus/plugin/vtspl/encoding/objective/NumberOfProductsObjective.java
@@ -0,0 +1,39 @@
+package org.nautilus.plugin.vtspl.encoding.objective;
+
+import org.nautilus.core.model.Instance;
+import org.nautilus.core.objective.AbstractObjective;
+import org.nautilus.plugin.vtspl.encoding.instance.AbstractTXTInstanceData;
+import org.uma.jmetal.solution.Solution;
+
+public class NumberOfProductsObjective extends AbstractObjective {
+	
+	protected int numbersOfProducts;
+	
+	@Override
+	public void beforeProcess(Instance instanceData, Solution<?> sol) {
+		this.numbersOfProducts = 0;
+	}
+	
+	@Override
+	public void process(Instance instanceData, Solution<?> sol, int i) {
+		numbersOfProducts++;
+	}
+	
+	@Override
+	public double calculate(Instance instanceData, Solution<?> sol) {
+		
+		AbstractTXTInstanceData instance = (AbstractTXTInstanceData) instanceData;
+		
+		return (double) numbersOfProducts / (double) instance.getNumberOfProducts();
+	}
+	
+	@Override
+	public String getName() {
+		return "Number of Products";
+	}
+
+	@Override
+	public String getGroupName() {
+		return "General";
+	}
+}
